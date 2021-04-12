efa8ef432fbf39d544462128b58494b50df40456
smile.kim <lachelngose@gmail.com>
2021-04-12 16:20:24 +0900

    feat. 1000미터 이내 가장 가까운 1개 역만 검색하도록 변경

--- Nearest/controller.py
+++ Nearest/controller.py
@@ -16,34 +16,23 @@ class Controller:
             DB_PORT=DB_PORT
         )
 
-    def get_article(self, pnu):
+    def get_article_coord(self, pnu: str):
         article = self.dal.session.query(Article).filter_by(pnu=pnu).first()
         coord_dict = dict()
         coord_dict["lat"] = str(article.latitude)
         coord_dict["lng"] = str(article.longitude)
+        return coord_dict
 
-        return article, coord_dict
-
-    def find_nearby_station(self, pnu):
+    def find_nearby_station(self, pnu: str):
         sql = ("SELECT s.id, s.line, s.name, s.lat, s.lng\n"
                "FROM article_master a\n"
                "LEFT JOIN station_coordinate s\n"
                "    ON ST_DWithin(\n"
                "          ST_Transform(ST_SetSRID(ST_MakePoint(a.longitude, a.latitude), 4326),5186), \n"
-               "          ST_Transform(ST_SetSRID(ST_MakePoint(s.lng, s.lat), 4326),5186), 3000) \n"
-               "WHERE a.pnu = \'{pnu}\'").format(pnu=pnu)
+               "          ST_Transform(ST_SetSRID(ST_MakePoint(s.lng, s.lat), 4326),5186), 1000) \n"
+               "WHERE a.pnu = \'{pnu}\'"
+               "ORDER BY ST_Distance(ST_MakePoint(a.longitude, a.latitude), ST_MakePoint(s.lng, s.lat))"
+               "LIMIT 1").format(pnu=pnu)
 
-        objs = self.dal.session.execute(sql)
-        stations = []
-        coord_dict_list = []
-        for obj in objs:
-            station = Station(obj[0], obj[1], obj[2], obj[3], obj[4])
-            stations.append(station)
-
-            coord_dict = dict()
-            coord_dict["station_id"] = station.id
-            coord_dict["lat"] = str(station.lat)
-            coord_dict["lng"] = str(station.lng)
-            coord_dict_list.append(coord_dict)
-
-        return stations, coord_dict_list
+        obj = self.dal.session.execute(sql).fetchone()
+        return Station(obj[0], obj[1], obj[2], obj[3], obj[4])
