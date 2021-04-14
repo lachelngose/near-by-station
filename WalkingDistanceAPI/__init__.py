b4db2957432d88f773387e568eb71384fa473cf9
smile.kim <lachelngose@gmail.com>
2021-04-14 13:17:06 +0900

    feat: T map API 반복 요청 시 403 응답이 반환되어, 5초 기다리고 재요청하도록 변경

--- WalkingDistanceAPI/__init__.py
+++ WalkingDistanceAPI/__init__.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 from WalkingDistanceAPI.config import API_KEY
 
@@ -9,8 +10,20 @@ BASE_URL = "https://apis.openapi.sk.com/tmap/routes/pedestrian"
 def make_request(payload: dict):
     params = {'version': '1', 'callback': 'json'}
     headers = {'appKey': API_KEY}
+    print(payload)
 
-    response = requests.post(BASE_URL, params=params, data=payload, headers=headers)
+    max_try_count = 5
+    try_count = 0
+
+    while try_count < max_try_count:
+        response = requests.post(BASE_URL, params=params, data=payload, headers=headers)
+        print(response.status_code)
+
+        if response.status_code / 100 == 2:
+            return response.json()
+        elif response.status_code == 403:
+            time.sleep(5)
+            try_count += 1
+
+    response.reason = response.text
     response.raise_for_status()
-
-    return response.json()
